8c9eb0fd1853045b1fe492b6ee72468616463ab9
Th3_N0c74m8u1L1s7 <56977232+k3czu405338@users.noreply.github.com>
2026-04-12 22:41:43 +0200

    Delete hc-sr04-distance-1.py

--- hc-sr04-distance-1.py
+++
@@ -1,9 +0,0 @@
-from gpiozero import DistanceSensor
-from time import sleep
-
-sensor = DistanceSensor(echo=24, trigger=23)
-
-while True:
-    distance = sensor.distance * 100  
-    print(f"Distance: {distance:.2f} cm")
-    sleep(0.1)
